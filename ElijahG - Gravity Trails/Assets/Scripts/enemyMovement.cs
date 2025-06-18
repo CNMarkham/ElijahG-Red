80457b0ce6660d0da651ae1d483bc4d197c53063
.(period) <markhamonca@codeninjas.com>
2025-06-18 18:29:20 -0400

    June 18, 2025 - Step 72
    Modifying Fire1 Method so it instantiate when the throwable is > 0 and afterwards throwableCounter-- . Lastly, destroy the object.

--- ElijahG - Gravity Trails/Assets/Scripts/enemyMovement.cs
+++ ElijahG - Gravity Trails/Assets/Scripts/enemyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class enemyMovement : MonoBehaviour
 {
     public float speed;
+    public int direction = 1;
 
     public int maximumXPosition;
     public int minimumXPosition;
@@ -18,13 +19,21 @@ public class enemyMovement : MonoBehaviour
     {
         if (transform.position.x <= minimumXPosition || transform.position.x >= maximumXPosition)
         {
+
             speed *= -1;
+        float newXScale = transform.localScale.x;
+        float newYScale = transform.localScale.y;
+        float newZScale = transform.localScale.z;
+        transform.localScale = new Vector3(newXScale *-1, newYScale, newZScale);
+
         }
 
         float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
         float newYPosition = transform.position.y;
+
         Vector3 newPosition = new Vector3(newXPosition, newYPosition, -4);
         transform.position = newPosition;
+
     }
 
 
