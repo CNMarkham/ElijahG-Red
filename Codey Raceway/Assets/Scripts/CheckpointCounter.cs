4545c63b7d850f73fe3e3d306d9edf31bca3ef17
Pookle444 <markhamonca@codeninjas.com>
2025-08-05 16:52:35 -0400

    Codey Raceway step 85

---
+++ Codey Raceway/Assets/Scripts/CheckpointCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointCounter : MonoBehaviour
+{
+    public int numberOfCheckpoints;
+    public int triggeredCheckpoints;
+    void Start()
+    {
+        numberOfCheckpoints = GameObject.FindGameObjectsWithTag("checkpoint").Length;
+    }
+
+}
