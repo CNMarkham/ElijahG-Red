96d336604b5d09c15cf6a08cad57978995397f1f
Chelsea <123781912+MariePlush@users.noreply.github.com>
2025-08-08 17:44:37 -0400

    Codey Raceway step 119

---
+++ Codey Raceway/Assets/Scripts/TimersCountdown.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimersCountdown : MonoBehaviour
+{
+    public Text lapTime;
+    public Text startCountdown;
+
+    public float totalLapTime;
+    public float totalCountdownTime;
+
+    public CodeyMove codeyMove;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+
+        if (totalCountdownTime > 0)
+        {
+            totalCountdownTime -= Time.deltaTime;
+            startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+            codeyMove.Speed = 0;
+        }
+
+        if (totalCountdownTime <= 0)
+        {
+            startCountdown.text = "";
+            totalLapTime -= Time.deltaTime;
+            lapTime.text = Mathf.Round(totalLapTime).ToString();
+            codeyMove.Speed = 300;
+        }
+
+        if (totalLapTime <= 0)
+        {
+            print("Time is up!");
+        }
+
+    }
+}
