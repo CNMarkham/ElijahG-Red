369f64a0313e7212e225f410e1e44fcd1781509e
Pookle444 <markhamonca@codeninjas.com>
2025-08-13 18:30:45 -0400

    Codey Raceway step 143

---
+++ Codey Raceway/Assets/Scripts/itemBoxSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxSpawner : MonoBehaviour
+{
+    public GameObject itemBox;
+
+    public int numberOfBoxes;
+
+    public int modifyXPosition;
+    public int modifyZPostion;
+
+    void Start()
+    {
+        // For loops
+        // The standard for loop has 3 components inside its paranthesis()
+        // They have the initializer, the condition and the increment, respectively
+        // The initializer state where the for loop begins,
+        // The condition says the condition where for loop ends
+        // The increment state how much the for loop increase by each time.
+        for (int i = 0; i < numberOfBoxes; i += 1)
+        {
+            GameObject itemBoxClone = Instantiate(
+                itemBox,
+                new Vector3(
+                    transform.position.x + modifyXPosition * i,
+                    transform.position.y,
+                    transform.position.z + modifyXPosition * i
+                    ),
+                transform.rotation);
+        }
+    }
+
+
+}
