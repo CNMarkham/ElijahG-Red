9ce5d7c608c86b1eb4763eb3a20e051756241a2c
.(period) <markhamonca@codeninjas.com>
2025-08-22 16:46:35 -0400

    Codey Raceway step 147

---
+++ Codey Raceway/Assets/Scripts/notes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notes : MonoBehaviour
+{
+
+    // accessor - public, private, protected
+    // type - int, Vector3, GameObject...
+    // name - ___
+}
